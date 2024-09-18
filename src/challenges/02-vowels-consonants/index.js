cfd462f11ac970b9549ab95e404d8e343816ed31
Anamaria Gherghisan <sanduanamaria44@yahoo.com>
2024-09-18 20:45:39 +0100

    practice vowels and Consonants

--- src/challenges/02-vowels-consonants/index.js
+++ src/challenges/02-vowels-consonants/index.js
@@ -1,32 +1,33 @@
-//input validation
-export const vowelsAndConsonants = (sentence) => {
+export const vowelAndConsonants = (sentence) => {
+  //validate sentence
   if (!sentence) {
     return "Error: Invalid sentence, should contain alphabetical characters at minimum";
   }
-
+  //convert the sentence into an array
   const sentenceArray = sentence.toLowerCase().split("");
-
   const hasAlphabets = sentenceArray.some((character) => {
-    const alphabets = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = "abcdefghijklmnopqrstuvxyzw";
     return alphabets.includes(character);
   });
 
   if (!hasAlphabets) {
     return "Error: Invalid sentence, should contain alphabetical characters at minimum";
   }
-
+  //initialize counters and define vowels
   let countVowels = 0;
   let countConsonants = 0;
   const vowels = "aeiou";
-  const consonants = "bcdfghjklmnpqrstvwxyz";
+  const consonants = "bcdfghjklmnpqrstvxyzw";
 
+  //process the sentence
   sentenceArray.forEach((character) => {
     if (vowels.includes(character)) {
       countVowels = countVowels + 1;
     }
+
     if (consonants.includes(character)) {
       countConsonants = countConsonants + 1;
     }
   });
-  return `Vowels: ${countVowels} |Consonants: ${countConsonants} `;
+  return "Vowels: ${countVowels} | Consonants: ${countConsonants}";
 };
--- src/challenges/02-vowels-consonants/index.test.js
+++ src/challenges/02-vowels-consonants/index.test.js
@@ -1,51 +1,51 @@
-import { vowelsAndConsonants } from ".";
-describe("vowelsAndConsonants", () => {
+import {vowelAndConsonants} from 
+describe("vowelAndConsonants", () => {
   it("should return expected message with vowels and consonants", () => {
     const expected = "Vowels: 5 | Consonants: 8";
-    const actual = vowelsAndConsonants("This is amazing");
+    const actual = vowelAndConsonants("This is amazing");
     expect(actual).toEqual(expected);
   });
   it("should return expected message with no vowels and consonants", () => {
     const expected = "Vowels: 0 | Consonants: 17";
-    const actual = vowelsAndConsonants("Try my dry rhythm cry");
+    const actual = vowelAndConsonants("Try my dry rhythm cry");
     expect(actual).toEqual(expected);
   });
   it("should return expected message with vowels and no consonants", () => {
     const expected = "Vowels: 11 | Consonants: 0";
-    const actual = vowelsAndConsonants("oo ooo ooouuu");
+    const actual = vowelAndConsonants("oo ooo ooouuu");
     expect(actual).toEqual(expected);
   });
   it("should return expected message when numbers are present", () => {
     const expected = "Vowels: 7 | Consonants: 9";
-    const actual = vowelsAndConsonants("Welcome to the year 2050");
+    const actual = vowelAndConsonants("Welcome to the year 2050");
     expect(actual).toEqual(expected);
   });
   it("should return expected message when special characters are present", () => {
     const expected = "Vowels: 5 | Consonants: 10";
-    const actual = vowelsAndConsonants("Dude where's my car?");
+    const actual = vowelAndConsonants("Dude where's my car?");
     expect(actual).toEqual(expected);
   });
   it("should return expected message when special characters and numbers are present", () => {
     const expected = "Vowels: 5 | Consonants: 12";
-    const actual = vowelsAndConsonants("Dude where's my 2nd car?");
+    const actual = vowelAndConsonants("Dude where's my 2nd car?");
     expect(actual).toEqual(expected);
   });
-  it("should return expected error message when no alphabets are present", () => {
+  it("should return expected error message  when no alphabets are present", () => {
     const expected =
       "Error: Invalid sentence, should contain alphabetical characters at minimum";
-    const actual = vowelsAndConsonants("#123");
+    const actual = vowelAndConsonants("#123");
     expect(actual).toEqual(expected);
   });
   it("should return expected error message when sentence is empty", () => {
     const expected =
       "Error: Invalid sentence, should contain alphabetical characters at minimum";
-    const actual = vowelsAndConsonants("");
+    const actual = vowelAndConsonants("");
     expect(actual).toEqual(expected);
   });
   it("should return expected error message when sentence is undefined", () => {
     const expected =
       "Error: Invalid sentence, should contain alphabetical characters at minimum";
-    const actual = vowelsAndConsonants();
+    const actual = vowelAndConsonants();
     expect(actual).toEqual(expected);
   });
 });
